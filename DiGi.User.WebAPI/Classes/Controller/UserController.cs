172efaaae848902a906e2c769fb944f8fa905889
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-09-08 08:44:03 +0200

    Code cleaning

--- DiGi.User.WebAPI/Classes/Controller/UserController.cs
+++ DiGi.User.WebAPI/Classes/Controller/UserController.cs
@@ -3,7 +3,6 @@ using DiGi.User.PostgreSQL.Classes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
